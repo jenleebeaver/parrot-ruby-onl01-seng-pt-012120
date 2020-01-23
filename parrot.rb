fd6961d6e8acbe8283a8311eb0f10687d3570ef9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-23 14:01:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,18 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+   puts "Squawk!"
+end
+
+def parrot
+ "Squawk!"
+end
+
+def parrot(phrase)
+  phrase = "Pretty bird!"
+  puts phrase
+end
+
+def parrot(phrase = "Pretty bird!")
+  phrase
+end
